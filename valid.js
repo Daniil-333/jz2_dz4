2d071cde75e7d39ff75d61cccc4846862e57e4ae
Daniel <monobootica@yandex.ru>
2021-02-25 10:49:07 +0500

    task3 RegExp is complete

---
+++ valid.js
@@ -0,0 +1,47 @@
+class Valid {
+
+        constructor(name, tel, email) {
+            this.name = name;
+            this.tel = tel;
+            this.email = email;
+            this.init();
+        }
+
+        init() {
+            this._checkInputName();
+            this._checkInputTel();
+            this._checkInputEmail();
+        }
+
+        _checkInputName () {
+            if (/^[a-zа-яА-ЯёЁ]+$/i.test(document.getElementById(this.name).value)) {
+                return;
+            }else {
+                document.getElementById(this.name).classList.add('mistake');
+                document.getElementById(this.name).insertAdjacentHTML('afterend', `<span class="error">Допустимые символы для ввода: <b>[a-z],[а-я],[А-Я],[ёЁ]</b></span>`);
+            }
+        }
+
+        _checkInputTel () {
+            if (/^\+7\({1}\d{3}\)\d{3}-\d{4}$/.test(document.getElementById(this.tel).value)) {
+                return;
+            }else {
+                document.getElementById(this.tel).classList.add('mistake');
+                document.getElementById(this.tel).insertAdjacentHTML('afterend', `<span class="error">Шаблон формата для ввода: <b>+7(000)000-0000</b></span>`);
+            }
+        }
+
+        _checkInputEmail () {
+            if (/^[a-z]{2,6}[.-]?[a-z]{4}@mail\.ru$/i.test(document.getElementById(this.email).value)) {
+                return;
+            }else {
+                document.getElementById(this.email).classList.add('mistake');
+                document.getElementById(this.email).insertAdjacentHTML('afterend', `<span class="error">E-mail может иметь вид <b>mymail@mail.ru</b>, или <b>my.mail@mail.ru</b>, или <b>my-mail@mail.ru</b></span>`);
+            }
+        }
+}
+document.querySelector('input[type=submit]').addEventListener('click', (event) => {
+    event.preventDefault();
+    new Valid('name', 'tel', 'email');
+    // document.getElementById(this.email).value.match();
+});
